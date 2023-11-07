fda02460c9b74de3506a32cc9817e91d4505cbab
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-11-07 22:27:22 +0900

    test: 그룹 참여 기능 test

--- src/test/java/cnu/healthcare/domain/group/service/GroupServiceImplTest.java
+++ src/test/java/cnu/healthcare/domain/group/service/GroupServiceImplTest.java
@@ -3,27 +3,26 @@ package cnu.healthcare.domain.group.service;
 import cnu.healthcare.domain.group.Group;
 import cnu.healthcare.domain.group.MemberGroup;
 import cnu.healthcare.domain.group.controller.dto.request.GroupDto;
+import cnu.healthcare.domain.group.controller.dto.request.JoinGroupDto;
 import cnu.healthcare.domain.group.controller.dto.response.GroupCodeRespDto;
 import cnu.healthcare.domain.group.repo.GroupRepository;
 import cnu.healthcare.domain.group.repo.MemberGroupRepository;
 import cnu.healthcare.domain.member.Member;
 import cnu.healthcare.domain.member.repo.MemberRepository;
+import cnu.healthcare.global.exception.handler.CustomApiException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.hamcrest.Matchers.any;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Transactional
@@ -42,14 +41,18 @@ public class GroupServiceImplTest {
     @Autowired
     private MemberGroupRepository memberGroupRepository;
 
-    @Test
-    public void testCreateGroup() {
-        // given
-        Member member = Member.builder()
+    Member member;
+    @BeforeEach
+    public void createInstance() {
+        member = Member.builder()
                 .memberId("test")
                 .build();
         memberRepository.save(member);
+    }
 
+    @Test
+    public void testCreateGroup() {
+        // given
         GroupDto groupDto = new GroupDto();
         groupDto.setMemberId("test");
         groupDto.setGroupName("Test Group");
@@ -59,16 +62,48 @@ public class GroupServiceImplTest {
         UUID groupCode = UUID.fromString(respDto.getGroupCode());
 
         Group group = groupRepository.findById(groupCode).get();
-        MemberGroup memberGroup = memberGroupRepository.findByGroup(group);
+        MemberGroup memberGroup = memberGroupRepository.findByGroupAndMember(group, member);
 
-        // 결과 확인
+        // then
         assertEquals(member.getMemberId(), memberGroup.getMember().getMemberId()
         );
     }
 
     @Test
-    public void UUID_GROUP_TEST() {
-        Group group = groupRepository.save(new Group("test"));
-        System.out.println(group.getGroupId());
+    public void GROUP_JOIN() {
+        // given
+        GroupDto groupDto = new GroupDto();
+        groupDto.setMemberId("test");
+        groupDto.setGroupName("Test Group");
+        GroupCodeRespDto respDto = groupService.createGroup(groupDto);
+
+        // when
+        Member newMember = Member.builder()
+                .memberId("test2")
+                .build();
+        memberRepository.save(newMember);
+
+        // then
+        JoinGroupDto alreadyJoinMember = new JoinGroupDto(respDto.getGroupCode(), member.getMemberId());
+        assertThrows(CustomApiException.class,
+                () -> groupService.joinGroup(alreadyJoinMember)
+        );
+        JoinGroupDto invalidGroupCode = new JoinGroupDto("invalid-UUID", newMember.getMemberId());
+        assertThrows(CustomApiException.class,
+                () -> groupService.joinGroup(invalidGroupCode)
+        );
+        JoinGroupDto correctJoin = new JoinGroupDto(respDto.getGroupCode(), newMember.getMemberId());
+        groupService.joinGroup(correctJoin);
+
+        Group group = groupRepository.findById(UUID.fromString(respDto.getGroupCode())).get();
+        MemberGroup newMemberGroup = memberGroupRepository.findByGroupAndMember(group, newMember);
+        System.out.println(newMemberGroup.getMember().getMemberId());
+        assertThat(memberGroupRepository.existsByGroupAndMember(group, newMember), is(true));
     }
+
+    // @Test
+    // public void UUID_GROUP_TEST() {
+    //     Group group = groupRepository.save(new Group("test"));
+    //     System.out.println(group.getGroupId());
+    // }
 }
