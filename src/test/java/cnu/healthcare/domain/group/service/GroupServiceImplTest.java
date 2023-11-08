9e202b40a38fabe37a05512de259f5c1add99602
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-11-08 16:58:15 +0900

    test: 내 그룹 정보 갖고오기 test

--- src/test/java/cnu/healthcare/domain/group/service/GroupServiceImplTest.java
+++ src/test/java/cnu/healthcare/domain/group/service/GroupServiceImplTest.java
@@ -5,6 +5,7 @@ import cnu.healthcare.domain.group.MemberGroup;
 import cnu.healthcare.domain.group.controller.dto.request.GroupDto;
 import cnu.healthcare.domain.group.controller.dto.request.JoinGroupDto;
 import cnu.healthcare.domain.group.controller.dto.response.GroupCodeRespDto;
+import cnu.healthcare.domain.group.controller.dto.response.MyGroupDto;
 import cnu.healthcare.domain.group.repo.GroupRepository;
 import cnu.healthcare.domain.group.repo.MemberGroupRepository;
 import cnu.healthcare.domain.member.Member;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -46,6 +48,7 @@ public class GroupServiceImplTest {
     public void createInstance() {
         member = Member.builder()
                 .memberId("test")
+                .memberName("test")
                 .build();
         memberRepository.save(member);
     }
@@ -100,6 +103,52 @@ public class GroupServiceImplTest {
         System.out.println(newMemberGroup.getMember().getMemberId());
         assertThat(memberGroupRepository.existsByGroupAndMember(group, newMember), is(true));
     }
+
+    @Test
+    public void getMyGroups() {
+        GroupDto groupDto = new GroupDto();
+        groupDto.setMemberId("test");
+        groupDto.setGroupName("Test Group");
+        GroupCodeRespDto respDto = groupService.createGroup(groupDto);
+
+        Member newMember = Member.builder()
+                .memberId("test2")
+                .memberName("1")
+                .build();
+        Member newMember2 = Member.builder()
+                .memberId("test3")
+                .memberName("2")
+                .build();
+        Member newMember3 = Member.builder()
+                .memberId("test4")
+                .memberName("3")
+                .build();
+        Member newMember4 = Member.builder()
+                .memberId("test5")
+                .memberName("4")
+                .build();
+        memberRepository.save(newMember);
+        memberRepository.save(newMember2);
+        memberRepository.save(newMember3);
+        memberRepository.save(newMember4);
+        JoinGroupDto join1 = new JoinGroupDto(respDto.getGroupCode(), newMember.getMemberId());
+        JoinGroupDto join2 = new JoinGroupDto(respDto.getGroupCode(), newMember2.getMemberId());
+        JoinGroupDto join3 = new JoinGroupDto(respDto.getGroupCode(), newMember3.getMemberId());
+        JoinGroupDto join4 = new JoinGroupDto(respDto.getGroupCode(), newMember4.getMemberId());
+        groupService.joinGroup(join1);
+        groupService.joinGroup(join2);
+        groupService.joinGroup(join3);
+        groupService.joinGroup(join4);
+
+        List<MyGroupDto> myGroups = groupService.getMyGroups(newMember2.getMemberId());
+        for(MyGroupDto myGroupDto : myGroups) {
+            System.out.println(myGroupDto.getGroupName());
+            myGroupDto.getMembers().stream()
+                    .forEach(System.out::println);
+        }
+
+    }
+
 
     // @Test
     // public void UUID_GROUP_TEST() {
